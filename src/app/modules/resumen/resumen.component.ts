2f98650c707c07e293816e49cf5272e7f476192f
LAPTOP-BGIF96EJ\usuario <desarrollador3@tecsersas.com>
2024-11-29 17:48:16 -0500

    Hoja de vida basica.

--- src/app/modules/resumen/resumen.component.ts
+++ src/app/modules/resumen/resumen.component.ts
@@ -49,30 +49,85 @@ export class ResumenComponent implements OnInit {
     ngOnInit(): void {
         this.horizontalStepperForm = this._formBuilder.group({
             step1: this._formBuilder.group({
-                fecha: ['', Validators.required],
-                country: ['', Validators.required],
-                language: ['', Validators.required],
+                fecha: [''],
+                country: [''],
+                language: [''],
             }),
             step2: this._formBuilder.group({
-                firstName: ['', Validators.required],
-                lastName: ['', Validators.required],
-                userName: ['', Validators.required],
-                about: [''],
+                nombre: ['', [Validators.maxLength(50)]],
+                cargo: ['', [Validators.maxLength(50)]],
+                correoElectronico: [
+                    '',
+                    [Validators.email],
+                ],
+                contacto: [
+                    '',
+                    [
+
+                        Validators.pattern('^[0-9]{10}$'),
+                    ],
+                ],
             }),
             step3: this._formBuilder.group({
-                byEmail: this._formBuilder.group({
-                    companyNews: [true],
-                    featuredProducts: [false],
-                    messages: [true],
-                }),
+                tipoEstrategiaIdentificacion: [''],
+                tipoPractica: [''],
+                codigoPractica: [{ value: '', disabled: true }],
+                tipologia: [{ value: ''}],
+                estadoFlujo: [{ value: 'Buena Practica Candidata', disabled: true }],
+                nivelBuenaPractica: [''],
+                nombreDescriptivo: ['', Validators.maxLength(100)],
+                propositoPractica: ['', Validators.maxLength(300)],
+                objetivoPrincipal: [''],
                 pushNotifications: ['everything', Validators.required],
             }),
-            // Nuevo paso para los campos adicionales
             newFields: this._formBuilder.group({
-                nombrePractica: ['', Validators.required],
-                identificacion: ['', Validators.required],
-                contenido: ['', Validators.required],
+                nombrePractica: [''],
+                identificacion: [''],
+                contenido: [''],
             }),
+            step4: this._formBuilder.group({
+                byEmail: this._formBuilder.group({
+                    companyNews: [false],
+                    featuredProducts: [false],
+                    messages: [false],
+                }),
+                pushNotifications: ['everything'],
+                impactoEsperado: this._formBuilder.array([]), 
+                metodologiaUsada: ['', [Validators.maxLength(500)]], 
+                duracionImplementacion: [''], 
+                etapasMetodologia: this._formBuilder.array([]), 
+                periodoDesarrollo: this._formBuilder.group({
+                    inicio: [''], 
+                    fin: [''], 
+                }),
+            }),
+            step5: this._formBuilder.group({
+                tipoMaterialProducido: this._formBuilder.array([], Validators.required), 
+                apoyoRecibido: this._formBuilder.array([], Validators.required), 
+                reconocimientos: ['', Validators.required], 
+                objetoControl: ['', Validators.required], 
+                taxonomiaEvento: this._formBuilder.array([], Validators.required), 
+                tipoActuacion: ['', Validators.required], 
+                documentoActuacion: ['', [Validators.pattern(/(\.pdf|\.docx|\.xlsx|\.pptx|\.zip)$/)]], // Carga de documentos
+                descripcionResultados: ['', Validators.required], 
+            }),            
         });
+    }
+    onPracticaChange(event: any): void {
+        const selectedValue = event.value;
+        const step3Form = this.horizontalStepperForm.get('step3');
+
+        if (selectedValue === 'BP') {
+            step3Form?.get('tipologia')?.enable();
+            step3Form?.get('codigoPractica')?.setValue('BP-' + this.generateConsecutive());
+        } else {
+            step3Form?.get('tipologia')?.disable();
+            step3Form?.get('codigoPractica')?.setValue('');
+        }
+    }
+
+    private generateConsecutive(): string {
+        const random = Math.floor(1000 + Math.random() * 9000);
+        return random.toString();
     }
 }
